75c9cdbaab5d48bbdf1e1f40aef75c6ec1ff3f67
RajatNikam <86477706+RajatNikam@users.noreply.github.com>
2021-10-27 16:28:49 +0530

    Add files via upload

---
+++ Experiences/logic.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
